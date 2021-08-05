bed84563fca8c3f53320953b1c6690052f892f22
user <rallakhverdiev.exceedteam@gmail.com>
2021-08-05 17:35:04 +0300

    create react app

---
+++ frontend-react-app/src/App.js
@@ -0,0 +1,20 @@
+import React, { useState, useEffect } from 'react';
+import {
+  Switch,
+  Route,
+  useHistory,
+  Link,
+  Redirect
+} from 'react-router-dom';
+
+import './App.scss';
+
+function App() {
+  return (
+    <div>
+      
+    </div>
+  );
+}
+
+export default App;
