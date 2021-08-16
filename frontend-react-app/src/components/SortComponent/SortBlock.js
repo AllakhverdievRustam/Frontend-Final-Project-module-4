e42ccce538d8e964a2eac0c02fac4d210c06f60d
user <rallakhverdiev.exceedteam@gmail.com>
2021-08-16 10:05:52 +0300

    fix mistakes in front

--- frontend-react-app/src/components/SortComponent/SortBlock.js
+++ frontend-react-app/src/components/SortComponent/SortBlock.js
@@ -4,31 +4,36 @@ import './SortBlock.scss';
 const SortBlock = ({ receptions, setReceptions }) => {
   const [sortLable, setSortLable] = useState('');
   const [sortDirection, setSortDirection] = useState('По возрастанию');
-  const sortArrLable = [{ lable: 'None', value: '' }, { lable: 'Имя', value: 'nameUser' }, { lable: 'Доктор', value: 'nameDoctor' }, { lable: 'Дата', value: 'date' }];
-  const sortArrDirection = ['По возрастанию', 'По убыванию'];
+  const sortArrLable = [
+    { lable: 'None', value: '' },
+    { lable: 'Имя', value: 'nameUser' },
+    { lable: 'Доктор', value: 'nameDoctor' },
+    { lable: 'Дата', value: 'date' }
+  ];
+  const sortArrDirection = [
+    { value: 'asc', title: 'По возрастанию' },
+    { value: 'desc', title: 'По убыванию' }
+  ];
 
   const onChangeLable = (e) => {
     if (e.target.value !== '') {
       receptions.sort((a, b) => a[e.target.value] > b[e.target.value] ? -1 : a[e.target.value] < b[e.target.value] ? 1 : 0);
       setReceptions([...receptions]);
     }
-    console.log(receptions);
     setSortLable(e.target.value);
   }
 
   const onChangeDirection = (e) => {
     if (sortLable !== '') {
       switch (e.target.value) {
-        case 'По возрастанию':
+        case 'asc':
           receptions.sort((a, b) => a[sortLable] > b[sortLable] ? -1 : a[sortLable] < b[sortLable] ? 1 : 0);
           setReceptions([...receptions]);
-          console.log(receptions);
           break;
 
-        case 'По убыванию':
+        case 'desc':
           receptions.sort((a, b) => a[sortLable] < b[sortLable] ? -1 : a[sortLable] > b[sortLable] ? 1 : 0);
           setReceptions([...receptions]);
-          console.log(receptions);
           break;
 
         default:
@@ -82,9 +87,9 @@ const SortBlock = ({ receptions, setReceptions }) => {
               sortArrDirection.map((element, index) => (
                 <option
                   key={`key-${index}`}
-                  value={element}
+                  value={element.value}
                 >
-                  {element}
+                  {element.title}
                 </option>
               ))
             }
